0ca2f718b1285b65df88d4ff12e31ba57815fbf0
Winter <92377024+phanquan0712@users.noreply.github.com>
2023-07-24 22:26:10 +0700

    update 24/7

--- lib/repositories/repository.dart
+++ lib/repositories/repository.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class Repository {
-  static const String baseUrl = 'http://localhost:5000/api';
+  static const String baseUrl = 'http://localhost:8000/api';
   final Map<String, String> headers = {
     HttpHeaders.contentTypeHeader: 'application/json',
   };
